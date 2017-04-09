fbfd53b156ddee3a4216e974100ab3f46c2d3c00
garyquiet <liugang_nj@163.com>
2017-04-09 19:42:09 +0800

    2

---
+++ Monitor/Monitor/MonitorDlg.cpp
@@ -0,0 +1,63 @@
+// MonitorDlg.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "Monitor.h"
+#include "MonitorDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CMonitorDlg 对话框
+
+CMonitorDlg::CMonitorDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(CMonitorDlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMonitorDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMonitorDlg, CDialog)
+#if defined(_DEVICE_RESOLUTION_AWARE) && !defined(WIN32_PLATFORM_WFSP)
+	ON_WM_SIZE()
+#endif
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+
+// CMonitorDlg 消息处理程序
+
+BOOL CMonitorDlg::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	// 设置此对话框的图标。当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+	
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+#if defined(_DEVICE_RESOLUTION_AWARE) && !defined(WIN32_PLATFORM_WFSP)
+void CMonitorDlg::OnSize(UINT /*nType*/, int /*cx*/, int /*cy*/)
+{
+	if (AfxIsDRAEnabled())
+	{
+		DRA::RelayoutDialog(
+			AfxGetResourceHandle(), 
+			this->m_hWnd, 
+			DRA::GetDisplayMode() != DRA::Portrait ? 
+			MAKEINTRESOURCE(IDD_MONITOR_DIALOG_WIDE) : 
+			MAKEINTRESOURCE(IDD_MONITOR_DIALOG));
+	}
+}
+#endif
+
---
+++ Monitor/Monitor/MonitorDlg.h
@@ -0,0 +1,30 @@
+// MonitorDlg.h : 头文件
+//
+
+#pragma once
+
+// CMonitorDlg 对话框
+class CMonitorDlg : public CDialog
+{
+// 构造
+public:
+	CMonitorDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_MONITOR_DIALOG };
+
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+#if defined(_DEVICE_RESOLUTION_AWARE) && !defined(WIN32_PLATFORM_WFSP)
+	afx_msg void OnSize(UINT /*nType*/, int /*cx*/, int /*cy*/);
+#endif
+	DECLARE_MESSAGE_MAP()
+};
