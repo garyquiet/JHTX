cfc24ac6e28586e1ce9b828cde06a0864445abc1
garyquiet <liugang_nj@163.com>
2017-04-09 19:47:44 +0800

    3

--- Monitor/Monitor/resource.h
+++ Monitor/Monitor/resource.h
@@ -4,7 +4,7 @@
 //
 #define IDD_MONITOR_DIALOG              102
 #define IDR_MAINFRAME                   128
-#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1001
 
 // Next default values for new objects
 // 
@@ -12,7 +12,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
