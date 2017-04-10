b883939bbdb54815c14ca0cdeca4175eb6d8ca11
garyquiet <liugang_nj@163.com>
2017-04-10 12:42:38 +0800

    ui

--- Monitor/Monitor/resource.h
+++ Monitor/Monitor/resource.h
@@ -4,15 +4,31 @@
 //
 #define IDD_MONITOR_DIALOG              102
 #define IDR_MAINFRAME                   128
+#define IDB_EMPTY_BITMAP                129
+#define IDB_FULL_BITMAP                 130
+#define IDB_LESS_BITMAP                 131
+#define IDB_MEDIA_BITMAP                132
+#define IDB_MORE_BITMAP                 133
+#define IDB_CONNECT_BITMAP              134
+#define IDB_DISCONNECT_BITMAP           135
 #define IDC_BUTTON2                     1001
+#define IDC_PRESET_INFO_BUTTON          1001
+#define IDC_SYSTEM_TIME_STATIC          1009
+#define IDC_SPECIAL_CODE_BUTTON         1010
+#define IDC_STATUS_STATIC               1011
+#define IDC_BATTERY_STATIC              1012
+#define IDC_SAVE_SETTING_BUTTON         1014
+#define IDC_BASE_STATION_BUTTON         1015
+#define IDC_WORKING_MODE_BUTTON         1016
+#define IDC_SYSTEM_SETTING_BUTTON       1017
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        136
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1013
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
